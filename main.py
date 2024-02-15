ec18aa50e9b0cce751bf495c1a8efa05b73e9f1d
Grant Nieda <grantnieda@gmail.com>
2024-02-15 00:13:05 -0800

    changes

--- main.py
+++ main.py
@@ -16,13 +16,21 @@ def get_geojson_for_state(state_initial):
     else:
         print("Error fetching data")
 
-
-def get_district_geojson_for_county(state_initial, county_name):
+def get_geojson_for_district(state_initial, district_number):
+    district_year = get_district_year(state_initial, district_number);
+    url = f"https://theunitedstates.io/districts/cds/{district_year}/{state_initial}-{district_number}/shape.geojson"
+    response = requests.get(url)
+    if response.status_code == 200:
+        geojson_data = response.json()
+        return geojson_data
+    else:
+        print("Error fetching data")
+def get_county_geojson(state_initial, county_name):
     district_number = get_district_number(state_initial, county_name)
     district_effect_year = get_district_year(state_initial, county_name)
 
     # Fetch the data from the repository
-    url = f"https://theunitedstates.io/districts/cds/{district_effect_year}/{state_initial}-{district_number}/shape.geojson"
+    url = "google.com"
     response = requests.get(url)
     if response.status_code == 200:
         geojson_data = response.json()
@@ -30,21 +38,18 @@ def get_district_geojson_for_county(state_initial, county_name):
     else:
         print("Error fetching data")
 
-
-def get_district_number(state_name, county_name):
+def get_district_number(state_initial, district_id):
     # Search and fetch data from a database or form
-    us_districts_df = pd.read_csv("./us_county_to_district.csv")  # Later may be possible to change this to gpt input but to limit the calls and price, this is a simple solution
-    print(us_districts_df.columns)
-
-    search_result = "01"
-    return search_result
-
+    df = pd.read_csv('us_districts.csv')
+    search = df[(df["district_id"] == district_id) & (df['state_initial'] == state_initial)]
+    if not search.empty:
+        return search.iloc[0]['year_in_effect']
+    else: return None
 
 def get_district_year(state_initial, district_number):
     # TODO: Search the database / form for the associated district year using state and number
     district_year = 2012
     return district_year
-
 
 # TODO: create a database with all district names for each state, year the district came into effect.
 # create a function that returns the county number based on the name.
@@ -55,9 +60,14 @@ def get_district_year(state_initial, district_number):
 
 
 # Example usage:
-state_name = "NY"
-county_name = "01"
-state_geojson = get_geojson_for_state(state_name)
-print(state_geojson)
-county_geojson = get_district_geojson_for_county(state_name, county_name)
-print(county_geojson)
+state_initial = "NY"
+district_num = "1"
+
+county_name = "Santa Clara"
+# state_geojson = get_geojson_for_state(state_initial)
+# print(state_geojson)
+
+district_geojson = get_geojson_for_district(state_initial, district_num)
+print(district_geojson)
+# county_geojson = get_district_geojson_for_county(state_name, county_name)
+# print(county_geojson)
